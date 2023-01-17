8fc5af3c7d199987cf47855cb4b88b048510aebe
정상준 <joungsangjun@jeongsangjun-ui-noteubug.local>
2023-01-17 13:54:37 +0900

    1weeks

--- 01.basics-compose/ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ 01.basics-compose/ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.composequadrant
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -10,8 +11,10 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
@@ -26,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    ComposeQuadrant()
+                    ComposeQuadrantApp()
                 }
             }
         }
@@ -34,24 +37,68 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ComposeQuadrant() {
-    Row() {
+fun ComposeQuadrantApp() {
+    Column() {
+        Row(modifier = Modifier.weight(1f)) {
+            ComposeQuadrantInfo(
+                mainContent = stringResource(R.string.main_text_composable),
+                subContent = stringResource(
+                    R.string.sub_text_composable
+                ),
+                backgroundColor = androidx.compose.ui.graphics.Color.Green,
+                modifier = Modifier.weight(1f)
+            )
 
+            ComposeQuadrantInfo(
+                mainContent = stringResource(R.string.main_image_compose),
+                subContent = stringResource(R.string.sub_image_compose),
+                backgroundColor = androidx.compose.ui.graphics.Color.Yellow,
+                modifier = Modifier.weight(1f)
+            )
 
+        }
+        Row(modifier = Modifier.weight(1f)) {
+            ComposeQuadrantInfo(
+                mainContent = stringResource(R.string.main_row_compose),
+                subContent = stringResource(R.string.sub_row_compose),
+                backgroundColor = androidx.compose.ui.graphics.Color.Cyan,
+                modifier = Modifier.weight(1f)
+            )
+
+            ComposeQuadrantInfo(
+                mainContent = stringResource(R.string.main_column_compose),
+                subContent = stringResource(R.string.sub_column_compose),
+                backgroundColor = androidx.compose.ui.graphics.Color.LightGray,
+                modifier = Modifier.weight(1f)
+            )
+        }
     }
 }
 
 @Composable
-private fun ComposableInfoCard(
-    title: String,
-    description: String
-)
-
+fun ComposeQuadrantInfo(mainContent: String, subContent: String, backgroundColor: Color, modifier: Modifier = Modifier) {
+    Column(modifier = modifier
+        .background(backgroundColor)
+        .fillMaxSize()
+        .padding(16.dp),
+    horizontalAlignment = Alignment.CenterHorizontally,
+    verticalArrangement = Arrangement.Center) {
+        Text(
+            text = mainContent,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        Text(
+            text = subContent,
+            textAlign = TextAlign.Justify
+        )
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeQuadrantTheme {
-        ComposeQuadrant()
+
     }
 }
