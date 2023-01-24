9430c54461ef0db3d43e32215f14833b45e37939
JoungSangJun <gogumi4502@nate.com>
2023-01-24 10:53:00 +0900

    Aniamtion add

--- 03.lists and Material Design/woof-MaterialTheming/app/src/main/java/com/example/woof/MainActivity.kt
+++ 03.lists and Material Design/woof-MaterialTheming/app/src/main/java/com/example/woof/MainActivity.kt
@@ -1,7 +1,7 @@
 /**
  * 2023년 1월 23일
  * Android-Developers
- * 단원 3: Material Theming
+ * 단원 3: Material Theming, Animation
  */
 package com.example.woof
 
@@ -10,21 +10,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -45,6 +44,34 @@ class MainActivity : ComponentActivity() {
                 WoofApp()
             }
         }
+    }
+}
+
+/**
+ * 강아지 취미를 출력
+ *
+ * @param dogHobby : 강아지 취미 resource ID
+ * @param modifier : 수정자
+ */
+
+@Composable
+fun DogHobby(@StringRes dogHobby: Int, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.padding(
+            start = 16.dp,
+            top = 8.dp,
+            bottom = 16.dp,
+            end = 16.dp
+        )
+    ) {
+        Text(
+            text = stringResource(id = R.string.about),
+            style = MaterialTheme.typography.h3
+        )
+        Text(
+            text = stringResource(dogHobby),
+            style = MaterialTheme.typography.body1
+        )
     }
 }
 
@@ -75,18 +102,57 @@ fun WoofApp() {
  */
 @Composable
 fun DogItem(dog: Dog, modifier: Modifier = Modifier) {
+    // true이면 DogHobby 펼쳐지고 false 이면 DogHobby 접혀있음
+    var expanded by remember { mutableStateOf(false) }
+
     Card(
         modifier = modifier.padding(8.dp),
         elevation = 4.dp
     ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
+        Column(
+            modifier = Modifier.animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
         ) {
-            DogIcon(dog.imageResourceId)
-            DogInformation(dog.name, dog.age)
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            ) {
+                DogIcon(dog.imageResourceId)
+                DogInformation(dog.name, dog.age)
+                Spacer(modifier = Modifier.weight(1f))
+                DogItemButton(expanded = expanded, onClick = { expanded = !expanded })
+            }
+            if (expanded) {
+                DogHobby(dogHobby = dog.hobbies)
+            }
         }
+    }
+}
+
+/**
+ * 화살표 아이콘 클릭시 이벤트처리
+ *
+ * @param expanded : DogHobby 사용 유무 확인
+ * @param onClick : 화살표 클릭시 발생 이벤트 람다처리
+ * @param modifier : 수정자
+ */
+@Composable
+private fun DogItemButton(
+    expanded: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    IconButton(onClick = onClick) {
+        Icon(
+            imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+            tint = MaterialTheme.colors.secondary,
+            contentDescription = stringResource(id = R.string.expand_button_content_description)
+        )
     }
 }
 
